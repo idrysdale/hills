b5e4b0cc3a2903c2b062144e253d0372ce2410de
Ian Drysdale <idrysdale@gmail.com>
2018-08-06 17:36:07 +0100

    Refactor to temporarily store activites in test

--- spec/activity_store_spec.rb
+++ spec/activity_store_spec.rb
@@ -1,18 +1,23 @@
+require "activity"
 require "activity_store"
+require "course"
+require "json"
 
 RSpec.describe ActivityStore do
-  let(:temporary_storage_directory) { 'tmp/activities' }
-  subject(:activity_store) {
-    ActivityStore.new(
-      temporary_storage_directory
-    )
-  }
-
-  before do
-    FileUtils.rm_rf("#{temporary_storage_directory}/.")
-  end
-
   describe '#push' do
+    let(:storage_directory) {
+      'spec/support/activities/tmp'
+    }
+    subject(:activity_store) {
+      ActivityStore.new(
+        storage_directory
+      )
+    }
+
+    before do
+      FileUtils.rm_rf("#{storage_directory}/.")
+    end
+
     context 'with new activity data' do
       let(:new_activity_data) {
         ActivityData.new(
@@ -28,17 +33,17 @@ RSpec.describe ActivityStore do
       end
 
       it 'writes a file to disk' do
-        files = Dir["#{temporary_storage_directory}/*"]
+        files = Dir["#{storage_directory}/*"]
         expect(files.count).to eq(1)
       end
 
       it 'names the json file after the activity id' do
-        file = Dir["#{temporary_storage_directory}/*"].first
+        file = Dir["#{storage_directory}/*"].first
         expect(File.basename(file)).to eq("#{new_activity_data.id}.json")
       end
 
       it 'writes the correct data' do
-        activity_file = Dir["#{temporary_storage_directory}/*"].first
+        activity_file = Dir["#{storage_directory}/*"].first
         activity_data_dump = File.read(activity_file)
         activity_data_hash = JSON.parse(activity_data_dump)
 
@@ -46,9 +51,9 @@ RSpec.describe ActivityStore do
         expect(activity_data_hash['name']).to eq(new_activity_data.name)
       end
     end
-  end
 
-  after do
-    FileUtils.rm_rf("#{temporary_storage_directory}")
+    after do
+      FileUtils.rm_rf("#{storage_directory}")
+    end
   end
 end
